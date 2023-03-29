dab3cd2bdabd76e4bc9c83b73e255d6cfda121e0
Hemraj <hemrajdangi2210@gmail.com>
2023-03-29 21:01:04 +0530

    Update __main__.py

--- DEADLY/__main__.py
+++ DEADLY/__main__.py
@@ -8,35 +8,35 @@ OWNER_ID = "GitExpert"
 
 if bot1:
     bot1.start()
-    bot1.join_chat("deadly_spambot")
+    bot1.join_chat("dangi_x")
 if bot2:
     bot2.start()
-    bot2.join_chat("deadly_spambot")
+    bot2.join_chat("dangi_x")
 if bot3:
     bot3.start()
-    bot3.join_chat("deadly_spambot")
+    bot3.join_chat("dangi_x")
 if bot4:
     bot4.start()
-    bot4.join_chat("deadly_spambot")
+    bot4.join_chat("dangi_x")
 if bot5:
     bot5.start()
-    bot5.join_chat("deadly_spambot")
+    bot5.join_chat("dangi_x")
 if bot6:
     bot6.start()
-    bot6.join_chat("deadly_spambot")
+    bot6.join_chat("dangi_x")
 if bot7:
     bot7.start()
-    bot7.join_chat("deadly_spambot")
+    bot7.join_chat("dangi_x")
 if bot8:
     bot8.start()
-    bot8.join_chat("deadly_spambot")
+    bot8.join_chat("dangi_x")
 if bot9:
     bot9.start()
-    bot9.join_chat("deadly_spambot")
+    bot9.join_chat("dangi_x")
 if bot10:
     bot10.start()
-    bot10.join_chat("deadly_spambot")
+    bot10.join_chat("dangi_x")
 
-print("Your Dangi Userbot Deployed Successfully 🥳 Please visit @Dangi_S for any help!") 
-
+print("Your Dangi Userbot 🦅🦅🦅🦅🦅🦅🦅Deployed Successfully 🥳 Please visit @Dangi_x for any help!") 
+print("Thanks for deploying dangi userbot🦅🦅🦅🦅🦅🦅🦅🦅🦅🦅🦅🦅🦅🦅🦅🦅🦅🦅🦅🦅🦅🦅🦅🦅🦅🦅🦅🦅🦅")
 idle()
